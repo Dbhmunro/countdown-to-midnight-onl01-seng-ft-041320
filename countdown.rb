47e0a6056da9e3b5451a1208898a4515fa5dcc78
David Munro <dbhmunro@gmail.com>
2020-04-14 19:59:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(n)
   while n > 0
-    n == 1 ? (puts "${n} SECOND!") : (puts "${n} SECONDS!")
+    n == 1 ? puts "${n} SECOND!" : puts "${n} SECONDS!"
     sleep(1)
     n -= 1
   end
